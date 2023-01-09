39c8d532659f92220f236f640a4db7022699a96f
Gregory Mitrano <gregory.mitrano@gmail.com>
2023-01-09 00:57:57 -0500

    Added Initial BC5 & BC4 Functions
    This change adds compress/decompress functions for BC5 & BC4. The BC5
    version is almost definitely broken in this change because I guessed
    about how the BC5 API works. This will be fixed in a future change when
    proper round-trip compress/decompress tests are added.

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,56 @@ pub fn compress_block_bc7(input_block: &[u8], output_block: &mut [u8]) -> bool {
     }
 }
 
+// FIXME: This is almost definitely broken
+pub fn compress_block_bc5(input_block: &[u8], output_block: &mut [u8]) -> bool {
+    unsafe {
+        sys::CompressBlockBC5(
+            input_block.as_ptr(),
+            8,
+            input_block.as_ptr().add(4),
+            8,
+            output_block.as_mut_ptr(),
+            core::ptr::null(),
+        ) == 0
+    }
+}
+
+pub fn compress_block_bc4(input_block: &[u8], output_block: &mut [u8]) -> bool {
+    unsafe {
+        sys::CompressBlockBC4(
+            input_block.as_ptr(),
+            4,
+            output_block.as_mut_ptr(),
+            core::ptr::null(),
+        ) == 0
+    }
+}
+
 pub fn decompress_block_bc7(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::DecompressBlockBC7(
+            input_block.as_ptr(),
+            output_block.as_mut_ptr(),
+            core::ptr::null(),
+        ) == 0
+    }
+}
+
+// FIXME: This is almost definitely broken
+pub fn decompress_block_bc5(input_block: &[u8], output_block: &mut [u8]) -> bool {
+    unsafe {
+        sys::DecompressBlockBC5(
+            input_block.as_ptr(),
+            output_block.as_mut_ptr(),
+            output_block.as_mut_ptr().add(4),
+            core::ptr::null(),
+        ) == 0
+    }
+}
+
+pub fn decompress_block_bc4(input_block: &[u8], output_block: &mut [u8]) -> bool {
+    unsafe {
+        sys::DecompressBlockBC4(
             input_block.as_ptr(),
             output_block.as_mut_ptr(),
             core::ptr::null(),
@@ -37,5 +84,33 @@ mod tests {
         let input_block = [0_u8; 16];
         let mut output_block = [0_u8; 64];
         assert!(decompress_block_bc7(&input_block, &mut output_block));
+    }
+
+    #[test]
+    fn compress_bc5() {
+        let input_block = [0_u8; 64];
+        let mut output_block = [0_u8; 16];
+        assert!(compress_block_bc5(&input_block, &mut output_block));
+    }
+
+    #[test]
+    fn decompress_bc5() {
+        let input_block = [0_u8; 16];
+        let mut output_block = [0_u8; 64];
+        assert!(decompress_block_bc5(&input_block, &mut output_block));
+    }
+
+    #[test]
+    fn compress_bc4() {
+        let input_block = [0_u8; 64];
+        let mut output_block = [0_u8; 16];
+        assert!(compress_block_bc4(&input_block, &mut output_block));
+    }
+
+    #[test]
+    fn decompress_bc4() {
+        let input_block = [0_u8; 16];
+        let mut output_block = [0_u8; 64];
+        assert!(decompress_block_bc4(&input_block, &mut output_block));
     }
 }
