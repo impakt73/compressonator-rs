1ddd1929c8f1f23712b3adf7bd507f993e94f389
Gregory Mitrano <gregory.mitrano@gmail.com>
2023-01-09 22:30:16 -0500

    Fix Byte Strides for BC5 & BC4
    This change attempts to fix some of the byte size and stride values used
    for the BC5 & BC4 code.

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,20 @@ pub fn compress_block_bc7(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::CompressBlockBC7(
             input_block.as_ptr(),
-            16,
+            64,
             output_block.as_mut_ptr(),
             core::ptr::null(),
         ) == 0
     }
 }
 
-// FIXME: This is almost definitely broken
 pub fn compress_block_bc5(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::CompressBlockBC5(
             input_block.as_ptr(),
-            8,
-            input_block.as_ptr().add(4),
-            8,
+            16,
+            input_block.as_ptr().add(16),
+            16,
             output_block.as_mut_ptr(),
             core::ptr::null(),
         ) == 0
@@ -29,7 +28,7 @@ pub fn compress_block_bc4(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::CompressBlockBC4(
             input_block.as_ptr(),
-            4,
+            16,
             output_block.as_mut_ptr(),
             core::ptr::null(),
         ) == 0
@@ -46,13 +45,12 @@ pub fn decompress_block_bc7(input_block: &[u8], output_block: &mut [u8]) -> bool
     }
 }
 
-// FIXME: This is almost definitely broken
 pub fn decompress_block_bc5(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::DecompressBlockBC5(
             input_block.as_ptr(),
             output_block.as_mut_ptr(),
-            output_block.as_mut_ptr().add(4),
+            output_block.as_mut_ptr().add(16),
             core::ptr::null(),
         ) == 0
     }
@@ -88,7 +86,7 @@ mod tests {
 
     #[test]
     fn compress_bc5() {
-        let input_block = [0_u8; 64];
+        let input_block = [0_u8; 32];
         let mut output_block = [0_u8; 16];
         assert!(compress_block_bc5(&input_block, &mut output_block));
     }
@@ -96,21 +94,21 @@ mod tests {
     #[test]
     fn decompress_bc5() {
         let input_block = [0_u8; 16];
-        let mut output_block = [0_u8; 64];
+        let mut output_block = [0_u8; 32];
         assert!(decompress_block_bc5(&input_block, &mut output_block));
     }
 
     #[test]
     fn compress_bc4() {
-        let input_block = [0_u8; 64];
-        let mut output_block = [0_u8; 16];
+        let input_block = [0_u8; 16];
+        let mut output_block = [0_u8; 8];
         assert!(compress_block_bc4(&input_block, &mut output_block));
     }
 
     #[test]
     fn decompress_bc4() {
-        let input_block = [0_u8; 16];
-        let mut output_block = [0_u8; 64];
+        let input_block = [0_u8; 8];
+        let mut output_block = [0_u8; 16];
         assert!(decompress_block_bc4(&input_block, &mut output_block));
     }
 }
