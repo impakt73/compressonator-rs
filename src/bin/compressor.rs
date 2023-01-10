b2af06dd12a6352ca0bb1617138c475f253e8ced
Gregory Mitrano <gregory.mitrano@gmail.com>
2023-01-09 23:16:51 -0500

    Added BC7 Image Compress/Decompress Functionality
    This change adds functions that allow round-tripping image files through
    BC7.

---
+++ src/bin/compressor.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+use compressonator::{compress_image_bc7, decompress_image_bc7, load_image_data};
+use image::{Rgb, RgbImage};
+
+pub fn main() {
+    let image_data = load_image_data("test.jpg").unwrap();
+    let compressed_data = compress_image_bc7(&image_data).unwrap();
+    let decompressed_data =
+        decompress_image_bc7(image_data.width, image_data.height, &compressed_data).unwrap();
+    let mut output_img = RgbImage::new(decompressed_data.width, decompressed_data.height);
+    for y in 0..(decompressed_data.height) {
+        for x in 0..(decompressed_data.width) {
+            let offset = ((y * decompressed_data.width + x) * 4) as usize;
+            let r = decompressed_data.pixels[offset];
+            let g = decompressed_data.pixels[offset + 1];
+            let b = decompressed_data.pixels[offset + 2];
+            output_img.put_pixel(x, y, Rgb([r, g, b]));
+        }
+    }
+    fs::write("output.tex", &compressed_data).unwrap();
+    output_img.save("output.jpg").unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use compressonator_sys as sys;
 
+use anyhow::Result;
+use image::io::Reader as ImageReader;
+
 pub fn compress_block_bc7(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::CompressBlockBC7(
             input_block.as_ptr(),
-            64,
+            16,
             output_block.as_mut_ptr(),
             core::ptr::null(),
         ) == 0
@@ -15,9 +18,9 @@ pub fn compress_block_bc5(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::CompressBlockBC5(
             input_block.as_ptr(),
-            16,
+            4,
             input_block.as_ptr().add(16),
-            16,
+            4,
             output_block.as_mut_ptr(),
             core::ptr::null(),
         ) == 0
@@ -28,7 +31,7 @@ pub fn compress_block_bc4(input_block: &[u8], output_block: &mut [u8]) -> bool {
     unsafe {
         sys::CompressBlockBC4(
             input_block.as_ptr(),
-            16,
+            4,
             output_block.as_mut_ptr(),
             core::ptr::null(),
         ) == 0
@@ -64,6 +67,108 @@ pub fn decompress_block_bc4(input_block: &[u8], output_block: &mut [u8]) -> bool
             core::ptr::null(),
         ) == 0
     }
+}
+
+pub struct RGBAImageData {
+    pub width: u32,
+    pub height: u32,
+    pub pixels: Vec<u8>,
+}
+
+pub fn load_image_data(path: &str) -> Result<RGBAImageData> {
+    let image = ImageReader::open(path)?.decode()?.into_rgba8();
+    let width = image.width();
+    let height = image.height();
+    let pixels = image.into_raw();
+
+    let image_data = RGBAImageData {
+        width,
+        height,
+        pixels,
+    };
+
+    Ok(image_data)
+}
+
+pub fn compress_image_bc7(image: &RGBAImageData) -> Result<Vec<u8>> {
+    assert!(image.width % 4 == 0);
+    assert!(image.height % 4 == 0);
+
+    let width_in_blocks = image.width / 4;
+    let height_in_blocks = image.height / 4;
+    let num_blocks = width_in_blocks * height_in_blocks;
+
+    let mut input_block = [0_u8; 64];
+    let mut output_block = [0_u8; 16];
+
+    let mut output_blocks = Vec::with_capacity((num_blocks * 16) as usize);
+
+    for block_y in 0..height_in_blocks {
+        for block_x in 0..width_in_blocks {
+            for y in 0..4 {
+                for x in 0..4 {
+                    let index =
+                        (((block_y * 4 + y) * image.width + (block_x * 4 + x)) * 4) as usize;
+                    let pixel = [
+                        image.pixels[index],
+                        image.pixels[index + 1],
+                        image.pixels[index + 2],
+                        image.pixels[index + 3],
+                    ];
+                    input_block[((y * 4 + x) * 4) as usize] = pixel[0];
+                    input_block[(((y * 4 + x) * 4) + 1) as usize] = pixel[1];
+                    input_block[(((y * 4 + x) * 4) + 2) as usize] = pixel[2];
+                    input_block[(((y * 4 + x) * 4) + 3) as usize] = pixel[3];
+                }
+            }
+
+            compress_block_bc7(&input_block, &mut output_block);
+            output_blocks.extend_from_slice(&output_block);
+        }
+    }
+
+    Ok(output_blocks)
+}
+
+pub fn decompress_image_bc7(width: u32, height: u32, data: &[u8]) -> Result<RGBAImageData> {
+    assert!(width % 4 == 0);
+    assert!(height % 4 == 0);
+
+    let width_in_blocks = width / 4;
+    let height_in_blocks = height / 4;
+    let num_blocks = width_in_blocks * height_in_blocks;
+
+    let mut input_block = [0_u8; 64];
+    let mut output_block = [0_u8; 16];
+
+    let mut pixels = vec![0; (num_blocks * 64) as usize];
+
+    for block_y in 0..height_in_blocks {
+        for block_x in 0..width_in_blocks {
+            let offset = ((block_y * width_in_blocks + block_x) * 16) as usize;
+            output_block.copy_from_slice(&data[offset..(offset + 16)]);
+
+            decompress_block_bc7(&output_block, &mut input_block);
+
+            for y in 0..4 {
+                for x in 0..4 {
+                    let index = (((block_y * 4 + y) * width + (block_x * 4 + x)) * 4) as usize;
+                    pixels[index] = input_block[((y * 4 + x) * 4) as usize];
+                    pixels[index + 1] = input_block[((y * 4 + x) * 4 + 1) as usize];
+                    pixels[index + 2] = input_block[((y * 4 + x) * 4 + 2) as usize];
+                    pixels[index + 3] = input_block[((y * 4 + x) * 4 + 3) as usize];
+                }
+            }
+        }
+    }
+
+    let image_data = RGBAImageData {
+        width,
+        height,
+        pixels,
+    };
+
+    Ok(image_data)
 }
 
 #[cfg(test)]
